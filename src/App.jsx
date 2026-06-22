65a3c74261a432690b191f7e4c2e0380e4634925
Basemgh99 <basemghanem3344@gmail.com>
2026-06-22 18:08:12 +0300

    create axios instance

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import router from './router'
 import { RouterProvider } from "react-router/dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 function App() {
 
   const queryClient=new QueryClient()
@@ -9,7 +10,9 @@ function App() {
   
 <>
        <QueryClientProvider client={queryClient}>
+      
     <RouterProvider router={router} />
+   
     </QueryClientProvider>
     </>  
     
--- src/hooks/useCategories.jsx
+++ src/hooks/useCategories.jsx
@@ -1,16 +1,10 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
+import React from "react"; 
+import axiosInstance from "../api/axiosinstance";
 function useCategories() {
   const getCategories = async () => {
-    const response = await axios.get(
-      `${import.meta.env.VITE_BURL}/Categories`,
-      {
-        headers: {
-          "Accept-Language": "en",
-        },
-      }
-    );
+    const response = await axiosInstance.get(`/Categories`,);
 
     return response.data;
   };
